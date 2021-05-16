7ec09dbdc42fe454fec6400462162c0a6d3a2d6d
Electromorphous <pstation.474@gmail.com>
2021-05-16 22:19:07 +0530

    ...

--- Program1/partB/lex.yy.c
+++ Program1/partB/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 7
-#define YY_END_OF_BUFFER 8
+#define YY_NUM_RULES 6
+#define YY_END_OF_BUFFER 7
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,10 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[12] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    8,    5,    6,    3,    4,    2,    1,    1,
-        0
+        0,    0,    7,    4,    5,    3,    2,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -403,28 +402,28 @@ static const YY_CHAR yy_meta[7] =
         1,    1,    1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[13] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    8,    9,    9,    9,    9,    9,    0,    0,
-        9,    5
+        0,    0,    8,    9,    9,    9,    9,    0,    0,    9,
+        5
     } ;
 
-static const flex_int16_t yy_def[13] =
+static const flex_int16_t yy_def[12] =
     {   0,
-       11,    1,   11,   11,   11,   11,   11,   11,   12,   12,
-        0,   11
+       10,    1,   10,   10,   10,   10,   10,   11,   11,    0,
+       10
     } ;
 
 static const flex_int16_t yy_nxt[16] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,    3,   11,
-       11,   11,   11,   11,   11
+        4,    5,    6,    6,    7,    8,    9,   10,    3,   10,
+       10,   10,   10,   10,   10
     } ;
 
 static const flex_int16_t yy_chk[16] =
     {   0,
-        1,    1,    1,    1,    1,    1,   12,    3,   11,   11,
-       11,   11,   11,   11,   11
+        1,    1,    1,    1,    1,    1,   11,    3,   10,   10,
+       10,   10,   10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -445,8 +444,8 @@ char *yytext;
 #line 2 "code1b.l"
     #include "y.tab.h"
     extern yylval;
+#line 448 "lex.yy.c"
 #line 449 "lex.yy.c"
-#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -666,7 +665,7 @@ YY_DECL
 #line 6 "code1b.l"
 
 
-#line 670 "lex.yy.c"
+#line 669 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,7 +692,7 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 12 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -746,25 +745,20 @@ YY_RULE_SETUP
 case 4:
 YY_RULE_SETUP
 #line 18 "code1b.l"
-{ return yytext[0]; }
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 19 "code1b.l"
 { ; }
 	YY_BREAK
-case 6:
-/* rule 6 can match eol */
+case 5:
+/* rule 5 can match eol */
 YY_RULE_SETUP
-#line 21 "code1b.l"
+#line 20 "code1b.l"
 { return 0; }
 	YY_BREAK
-case 7:
+case 6:
 YY_RULE_SETUP
-#line 23 "code1b.l"
+#line 22 "code1b.l"
 ECHO;
 	YY_BREAK
-#line 768 "lex.yy.c"
+#line 762 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1061,7 +1055,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 12 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1089,11 +1083,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 12 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 11);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1769,6 +1763,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 23 "code1b.l"
+#line 22 "code1b.l"
 
 
