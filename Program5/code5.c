c05b422b1c0b55006d6b57de7f5a2905a88f7f54
Electromorphous <pstation.474@gmail.com>
2021-06-11 18:57:20 +0530

    add program 5

---
+++ Program5/code5.c
@@ -0,0 +1,61 @@
+/*
+
+    Design, develop and implement a C/Java program to
+    generate the machine code using Triples for the statement
+    A = -B * (C +D) whose intermediate code in three-address form:
+
+    T1 = -B
+    T2 = C + D
+    T3 = T1 * T2
+    A = T3
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+char op[2], arg1[5], arg2[5], result[5];
+
+void main()
+{
+    FILE *fp1, *fp2;
+    fp1 = fopen("InputFile.txt", "r");
+    fp2 = fopen("OutputFile.txt", "w");
+    for (;;)
+    {
+        fscanf(fp1, "%s%s%s%s", result, arg1, op, arg2);
+        if (feof(fp1))
+            break;
+        if (strcmp(op, "+") == 0)
+        {
+            fprintf(fp2, "MOV R0,%s\n", arg1);
+            fprintf(fp2, "ADD R0,%s\n", arg2);
+            fprintf(fp2, "MOV %s,R0\n", result);
+        }
+        if (strcmp(op, "*") == 0)
+        {
+            fprintf(fp2, "MOV R0,%s\n", arg1);
+            fprintf(fp2, "MUL R0,%s\n", arg2);
+            fprintf(fp2, "MOV %s,R0\n", result);
+        }
+        if (strcmp(op, "-") == 0)
+        {
+            fprintf(fp2, "MOV R0,%s\n", arg1);
+            fprintf(fp2, "SUB R0,%s\n", arg2);
+            fprintf(fp2, "MOV %s,R0\n", result);
+        }
+        if (strcmp(op, "/") == 0)
+        {
+            fprintf(fp2, "MOV R0,%s\n", arg1);
+            fprintf(fp2, "DIV R0,%s\n", arg2);
+            fprintf(fp2, "MOV %s,R0\n", result);
+        }
+        if (strcmp(op, "=") == 0)
+        {
+            fprintf(fp2, "MOV R0,%s\n", arg1);
+            fprintf(fp2, "MOV %s,R0\n", result);
+        }
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
